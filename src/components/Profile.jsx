8d2f92e7b164b811f7b357cc88a63cc931f93fc5
Kartik <kartik.ipatil10@gmail.com>
2021-02-18 22:12:27 +0530

    Created profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import addImage from '../media/addImage.png';
+import profilePicture from '../media/profilePic.jpg';
+
 const Profile = () => {
+    let img = 'https://m.media-amazon.com/images/M/MV5BMzQxNjk5OTEtODU4ZC00OGE3LWIzM2YtYWIyNzhjNGFkZjA3XkEyXkFqcGdeQXVyNzg3NjQyOQ@@._V1_.jpg';
     return (
         <div className="profile">
-            <h1>Profile</h1>
+            <div className="sec-one">
+                <div className="secone-text">
+                    <p>Favorites</p>
+                    <i class="fas fa-edit"></i>
+                </div>
+                <div className="secone-img">
+                    <img src={ img ? img : addImage } alt="" />
+                    <img src={ img ? img : addImage } alt="" />
+                    <img src={ addImage } alt="" />
+                </div>
+            </div>
+            <div className="sec-two">
+                <img src={profilePicture} alt={profilePicture} />
+                <p>kartik patil</p>
+            </div>
         </div>
     )
 }
